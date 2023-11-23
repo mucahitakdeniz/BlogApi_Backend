56221fa3eb5aeaf6e08d4c89d9871d1b9f7356a9
mucahit <mucahitakdeniz28@gmail.com>
2023-11-23 12:44:34 +0300

    errorHandler

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const HOST = process.env?.HOST || "127.0.0.1";
+const PORT = process.env?.PORT || 8000;
+
+//error handler for async errors
+require("express-async-errors");
+
+// Connect to DB:
+const {dbConnection} =require('./src/configs/dbConnection')
+dbConnection()
+
+//Accept JSON
+app.use(express.json())
+
+
+
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+//routes
+
+//error handler
+app.use(require('./src/middlewares/errorHandler'))
+app.listen(PORT, () => console.log(`${PORT}`));
